94fe68f9155a32265a36d76d3f0645845272b7c0
leonid3101 <leonid2015@yandex.ru>
2020-12-28 06:59:14 +0300

    Task 1,2 (day 4).

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,7 +1,41 @@
 package day4;
 
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int size = scanner.nextInt();
+        int sum = 0;
+        int count1, count2, count3, count4;
+        count1 = count2 = count3 = count4 = 0;
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = rand.nextInt(10);
+            sum = sum + array[i];
+            if (array[i] > 8) {
+                count1++;
+            } else if (array[i] == 1) {
+                count2++;
+            }
+            if (array[i] % 2 == 0) {
+                count3++;
+            } else {
+                count4++;
+            }
 
+        }
+        System.out.print(Arrays.toString(array));
+        System.out.println();
+        System.out.println(size);
+        System.out.println(count1);
+        System.out.println(count2);
+        System.out.println(count3);
+        System.out.println(count4);
+        System.out.println(sum);
     }
 }
--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,34 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random rand = new Random();
+        int[] array = new int[100];
+        int sum =0;
+        int count =0;
+        int min = array[0];
+        int max = array[0];
+        for (int i = 0; i < 100; i++) {
+            array[i] = rand.nextInt(10000);
+                if (array[i] < min){
+                    min =array[i];
+                } else if (array[i]>max){
+                    max = array[i];
+                }
+                if ((array[i]%10)==0){
+                    count++;
+                    sum = sum + array[i];
+
+                }
+            }
+        System.out.println(Arrays.toString(array));
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(count);
+        System.out.println(sum);
+        }
 
     }
-}
