eeaabe438bb1cf2291b35b7359592a4d79d9f78b
leonid3101 <leonid2015@yandex.ru>
2020-12-29 23:30:19 +0300

    Task 1,2 (day 5).

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,10 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setColor("Black");
+        car.setModel("BMW");
+        car.setReleaseYear(2015);
+        System.out.println(car.toString());
     }
 }
