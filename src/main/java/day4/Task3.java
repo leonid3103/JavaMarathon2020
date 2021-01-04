aa39f698496c42b3d0e9460fdea67672024115bf
leonid3101 <leonid2015@yandex.ru>
2021-01-04 22:58:24 +0300

    Task 3 (day 4).

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,29 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task3 {
     public static void main(String[] args) {
+        Random rand = new Random();
+        int[][] array = new int [12][8];
+        int sumMax = 0;
+        int sumStr = 0;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = rand.nextInt(50);
+                sumStr = sumStr + array[i][j];
+                if (sumStr > sumMax) {
+                    sumMax = sumStr;
+                    index = i;
+                } else if (sumStr == sumMax) {
+                    index = i;
+                }
+            }
+            sumStr = 0;
 
+        }
+        System.out.println("Максимальная сумма " + sumMax + " в строке с индексом " + index);
     }
 }
