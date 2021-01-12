eacd4d511c4cbb323c58c4a06f1ad448ab08c985
leonid3101 <leonid2015@yandex.ru>
2021-01-12 23:01:06 +0300

    Task 1 (day 14).

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,30 @@
 package day14;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("Task 14-1");
+        printSumDigits(file);
+    }
+
+    public static void printSumDigits(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        int counter = 0;
+        int sum = 0;
+
+        String[] values = line.split(" ");
+
+          if (values.length != 10) {
+            throw new AssertionError("Некорректный входной файл");
+        }
+        for (String val : values) {
+            sum = sum + Integer.parseInt(val);
+
+        }
+        System.out.println(sum);
     }
 }
