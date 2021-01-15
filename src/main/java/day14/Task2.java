1623f44e02034b015d38c076ec925c86fc83f6be
leonid3101 <leonid2015@yandex.ru>
2021-01-15 23:24:02 +0300

    Task 2,3 (day 14).

--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,31 @@
 package day14;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
+public class Task2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("Task 14-2");
+        System.out.println(parseFileToStringList(file));
+    }
+
+    public static List<String> parseFileToStringList(File file) throws FileNotFoundException, AssertionError {
+        List<String> names = new ArrayList<>();
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] values = line.split(" ");
+            for (String val : values) {
+                val = values[1];
+                if (Integer.parseInt(val) < 0) {
+                    names = null;
+                    throw new AssertionError("Некорректный входной файл");
+                }
+            }
+            names.add(line);
+        }
+        scanner.close();
+        return names;
     }
 }
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,31 @@
 package day14;
 
-public class Task3 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Task3 {
+
+    public static void main(String[] args) throws FileNotFoundException, AssertionError {
+        File file = new File("Task 14-3");
+        System.out.println(parseFileToObjList(file));
+    }
+
+    public static List<Person> parseFileToObjList(File file) throws FileNotFoundException, AssertionError {
+        List<Person> persons = new ArrayList<>();
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] values = line.split(" ");
+            if (Integer.parseInt(values[1]) < 0) {
+                persons = null;
+                throw new AssertionError("Некорректный входной файл");
+            }
+            persons.add(new Person(values[0], Integer.parseInt(values[1])));
+        }
+        scanner.close();
+        return persons;
     }
 }
